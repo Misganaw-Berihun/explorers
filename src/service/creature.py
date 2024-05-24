21e22f7fd2caad96f7ef3acbc24edef70c9c50f1
Misganaw-Berihun <misganawbmb@gmail.com>
2024-05-24 22:52:37 +0300

    service layer

---
+++ src/service/creature.py
@@ -0,0 +1,26 @@
+from model.creature import Creature
+import data.creature as data
+
+
+def get_all() -> list[Creature]:
+    return data.get_all()
+
+
+def get_one(name: str) -> Creature | None:
+    return data.get_one(name)
+
+
+def create(creature: Creature) -> Creature:
+    return data.create(creature)
+
+
+def replace(id, creature: Creature) -> Creature:
+    return data.replace(id, creature)
+
+
+def modify(creature: Creature) -> Creature:
+    return data.modify(creature)
+
+
+def delete(name: str) -> bool:
+    return data.delete(name)
